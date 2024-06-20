7d4366030a461ed8025c286c6b19e5a307cec28e
yugpunjabi <yugpunjabi14@gmail.com>
2024-06-20 16:35:54 +0530

    Add files via upload

---
+++ models/listing.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review");
+const { required } = require("joi");
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    url: String,
+    filename: String,
+  },
+  price: Number,
+  location: String,
+  country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  geometry: {
+    type: {
+      type: String, // Don't do `{ location: { type: String } }`
+      enum: ["Point"], // 'location.type' must be 'Point'
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
+  category: {
+    type: String,
+    enum: [
+      "rooms",
+      "iconic_city",
+      "mountains",
+      "castles",
+      "amazing_pool",
+      "camping",
+      "farm",
+      "arctic",
+    ],
+    required: true,
+  },
+  trending: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+});
+
+// Handling Deletion
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing.reviews.length) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
